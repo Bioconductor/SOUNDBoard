a9aff2158267a6f15fb6da7efeb369a1e328963a
LiNk-NY <mramos09@gmail.com>
2017-03-28 17:17:34 -0400

    Updates to ShinyWidget class and vignette

--- R/ShinyWidget-class.R
+++ R/ShinyWidget-class.R
@@ -1,23 +1,30 @@
+.ShinyAppWidget <- setClass("ShinyAppWidget", contains = "SOUNDWidget")
 #' @exportClass ShinyAppWidget
 #'
 #' @export
 ShinyAppWidget <- SOUNDWidget(
-    "ShinyAppWidget",
+    widget = "ShinyAppWidget",
     save = function(x, file) saveRDS(x, file),
     load = function(x, file) readRDS(file),
     report = function(x) sbresource(x)
 )
 
+.ShinyAppDirWidget <- setClass("ShinyAppDirWidget", contains = "SOUNDWidget")
 #' @exportClass ShinyAppDirWidget
 #'
 #' @export
 ShinyAppDirWidget <- SOUNDWidget(
-    "ShinyAppDirWidget",
+    widget = "ShinyAppDirWidget",
     save = function(x, file) {
         ## create a tar ball of the app at 'file'
+        tar(file.path(file, "ShinyAppDirWidget.tar"), files = dir(file))
     },
     load = function(x, file) {
         ## untar file to temporary directory (where?) and return directory path
+        shinyOut <- system.file("shiny", basename(file), package = "SOUNDBoard")
+        untar(file.path(file, "ShinyAppDirWidget.tar"),
+              exdir = shinyOut)
+        shinyOut
     },
     report = function(x) sbresource(x)         # FIXME: ???
 )
