668d60f874f16358da8363451849d51d54546d85
LiNk-NY <mramos09@gmail.com>
2017-03-29 20:25:03 -0400

    make shiny classes avail in topenv

--- R/ShinyWidget-class.R
+++ R/ShinyWidget-class.R
@@ -1,4 +1,3 @@
-.ShinyAppWidget <- setClass("ShinyAppWidget", contains = "SOUNDWidget")
 #' @exportClass ShinyAppWidget
 #'
 #' @export
@@ -6,25 +5,33 @@ ShinyAppWidget <- SOUNDWidget(
     widget = "ShinyAppWidget",
     save = function(x, file) saveRDS(x, file),
     load = function(x, file) readRDS(file),
-    report = function(x) sbresource(x)
+    report = function(x) sbresource(x),
+    where = topenv()
 )
 
-.ShinyAppDirWidget <- setClass("ShinyAppDirWidget", contains = "SOUNDWidget")
 #' @exportClass ShinyAppDirWidget
 #'
 #' @export
 ShinyAppDirWidget <- SOUNDWidget(
     widget = "ShinyAppDirWidget",
     save = function(x, file) {
+        if (!dir.exists(file))
+            dir.create(file, recursive = TRUE)
         ## create a tar ball of the app at 'file'
-        tar(file.path(file, "ShinyAppDirWidget.tar"), files = dir(file))
+        tar(file.path(file, "ShinyAppDirWidget.tar.gz"),
+            files = list.files(sbresource(x), full.names = TRUE),
+            compression = "gzip")
     },
     load = function(x, file) {
-        ## untar file to temporary directory (where?) and return directory path
-        shinyOut <- system.file("shiny", basename(file), package = "SOUNDBoard")
-        untar(file.path(file, "ShinyAppDirWidget.tar"),
+        pkgPath <- file.path(system.file("shiny", package = "SOUNDBoard"))
+        shinyOut <- file.path(pkgPath, basename(file))
+        if (!dir.exists(shinyOut))
+            dir.create(shinyOut, recursive = TRUE)
+        ## untar file to shiny directory and return directory path
+        untar(file.path(file, "ShinyAppDirWidget.tar.gz"),
               exdir = shinyOut)
-        shinyOut
+        shinyOut ## TODO: Fix Me
     },
-    report = function(x) sbresource(x)         # FIXME: ???
+    report = function(x) shiny::shinyAppDir(sbresource(x)),
+    where = topenv()
 )
